baa4b0c4d4504a292560ded15f93f4a99c9ef42b
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2023-04-17 16:37:58 -0700

    added fetch error handling, fetch loading feedback, reset prediction state on draw

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -5,7 +5,8 @@ import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 const Canvas = ({ mousedown }: { mousedown: boolean }) => {
   const n = 28;
-  const [prediction, setPrediction] = useState<number | null>(null);
+  const [prediction, setPrediction] = useState<number | "loading" | null>(null);
+  const [predictionError, setPredictionError] = useState<string | null>(null);
 
   const [canvas, setCanvas] = useState<boolean[][]>(
     new Array(n).fill(new Array(n).fill(false))
@@ -16,11 +17,14 @@ const Canvas = ({ mousedown }: { mousedown: boolean }) => {
       let nextCanvas = canvas.map((r) => r.slice());
       nextCanvas[row][col] = true;
       setCanvas(nextCanvas);
+      setPrediction(null);
     }
   };
 
   const handleSubmit = () => {
     const getPrediction = async () => {
+      setPredictionError(null);
+      setPrediction("loading");
       try {
         const response = await fetch(
           "http://127.0.0.1:5000/api/model/predict",
@@ -33,7 +37,8 @@ const Canvas = ({ mousedown }: { mousedown: boolean }) => {
         const responseJSON = await response.json();
         setPrediction(responseJSON);
       } catch (e) {
-        console.log(e);
+        setPredictionError("An unknown error occurred. Please try again.");
+        setPrediction(null);
       }
     };
     getPrediction();
@@ -44,8 +49,8 @@ const Canvas = ({ mousedown }: { mousedown: boolean }) => {
   };
 
   return (
-    <div className="flex flex-col space-y-[10px]">
-      <div className="border-[2px] border-gray-300 rounded">
+    <div className="flex flex-col space-y-[10px] items-center">
+      <div className="border-[2px] border-gray-300 rounded cursor">
         {canvas.map((r, row) => (
           <div className="flex" key={`row-${row}`}>
             {r.map((filled, col) => (
@@ -64,7 +69,17 @@ const Canvas = ({ mousedown }: { mousedown: boolean }) => {
           className="flex-1 border-[2px] border-emerald-500 text-emerald-500 bg-emerald-50 px-[10px] py-[2px] font-bold rounded
             hover:bg-emerald-100"
         >
-          submit
+          {prediction === "loading" ? (
+            <>
+              <div
+                className="mr-[5px] inline-block h-[12px] w-[12px] animate-spin rounded-full border-[3px] border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                role="status"
+              />
+              loading...
+            </>
+          ) : (
+            "submit"
+          )}
         </button>
         <button
           className="border-[2px] border-red-500 bg-red-50 px-[10px] py-[2px] font-bold rounded
@@ -74,7 +89,10 @@ const Canvas = ({ mousedown }: { mousedown: boolean }) => {
           <FontAwesomeIcon className="text-red-500" icon={faTrash} />
         </button>
       </div>
-      {prediction && <h3>predicted value: {prediction}</h3>}
+      {prediction && prediction !== "loading" && (
+        <h3>predicted value: {prediction}</h3>
+      )}
+      {predictionError && <h3 className="text-red-500">{predictionError}</h3>}
     </div>
   );
 };
